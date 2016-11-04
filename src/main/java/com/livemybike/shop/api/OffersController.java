ecbe3fd9779a21999b0ae743417083f4d084146b
Diyan Yordanov <dgyordanov@gmail.com>
2016-11-04 11:16:49 +0100

    Implement the offers filter

--- src/main/java/com/livemybike/shop/api/OffersController.java
+++ src/main/java/com/livemybike/shop/api/OffersController.java
@@ -2,13 +2,14 @@ package com.livemybike.shop.api;
 
 import com.livemybike.shop.entities.Offer;
 import com.livemybike.shop.repos.OffersRepo;
-import com.livemybike.shop.service.OfferDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -32,8 +33,16 @@ public class OffersController {
     private OffersRepo offersRepo;
 
     @RequestMapping(value = "", method = GET, produces = APPLICATION_JSON_VALUE)
-    public Iterable<Offer> read() {
-        return offersRepo.findAll();
+    public Iterable<Offer> read(@RequestParam(value = "gender", required = false) String genderFilter) {
+        if (StringUtils.isEmpty(genderFilter)) {
+            // Not filter passed. Return everything.
+            return offersRepo.findAll();
+        } else {
+            List<String> filters = genderFilter.chars()
+                    .mapToObj(c -> String.valueOf((char) c))
+                    .collect(Collectors.toList());
+            return offersRepo.findByGenderIn(filters);
+        }
     }
 
 
--- src/main/java/com/livemybike/shop/repos/OffersRepo.java
+++ src/main/java/com/livemybike/shop/repos/OffersRepo.java
@@ -3,5 +3,11 @@ package com.livemybike.shop.repos;
 import com.livemybike.shop.entities.Offer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface OffersRepo extends CrudRepository<Offer, Long> {
+
+    List<Offer> findByGenderIn(Collection<String> genders);
+
 }
