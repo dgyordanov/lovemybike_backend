18aa1635b75f2cacc5d5dea2cb1bc162226e9011
Diyan Yordanov <dgyordanov@gmail.com>
2016-11-15 22:25:26 +0100

    X-Requested-By header should prevent the WWW-Authenticate response header which opens the browser popup

--- src/main/java/com/livemybike/shop/security/WebSecurityConfig.java
+++ src/main/java/com/livemybike/shop/security/WebSecurityConfig.java
@@ -5,6 +5,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -13,8 +20,22 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-                httpBasic().and().
-                csrf().disable();
+                httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response
+                    , AuthenticationException authException) throws IOException, ServletException {
+                String requestedBy = request.getHeader("X-Requested-By");
+                if (requestedBy == null || requestedBy.isEmpty()) {
+                    HttpServletResponse httpResponse = (HttpServletResponse) response;
+                    httpResponse.addHeader("WWW-Authenticate", "Basic realm=Cascade Realm");
+                    httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+                } else {
+                    HttpServletResponse httpResponse = (HttpServletResponse) response;
+                    httpResponse.addHeader("WWW-Authenticate", "Application driven");
+                    httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+                }
+            }
+        }).and().csrf().disable();
     }
 
     @Override
