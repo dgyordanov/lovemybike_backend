1a45ab1f3befd42159a8e3e739b5860239687dd7
Diyan Yordanov <dgyordanov@gmail.com>
2017-04-22 23:41:23 +0200

    Allow view an offer when user not logged in

--- src/main/java/com/livemybike/shop/security/WebSecurityConfig.java
+++ src/main/java/com/livemybike/shop/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] UNRESTRICTED_URIS = new String[] {
-            "/", "/console/**", "/offers", "/account", "/assets/**", "/css/**", "/img/**", "/favicon.ico"
+            "/", "/console/**", "/offers/**", "/account", "/assets/**", "/css/**", "/img/**", "/favicon.ico"
     };
 
     @Override
@@ -30,14 +30,12 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint() {
             @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+            public void commence(HttpServletRequest request, HttpServletResponse httpResponse, AuthenticationException authException) throws IOException, ServletException {
                 String requestedBy = request.getHeader("X-Requested-By");
                 if (requestedBy == null || requestedBy.isEmpty()) {
-                    HttpServletResponse httpResponse = (HttpServletResponse) response;
                     httpResponse.addHeader("WWW-Authenticate", "Basic realm=Cascade Realm");
                     httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
                 } else {
-                    HttpServletResponse httpResponse = (HttpServletResponse) response;
                     httpResponse.addHeader("WWW-Authenticate", "Application driven");
                     httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
                 }
